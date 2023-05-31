fc767768164b185cb3633b8e1918ac0acc8240a8
Stefano Verna <s.verna@datocms.com>
2023-05-31 14:00:01 +0200

    foo

--- pages/blog/index.js
+++
@@ -1,8 +0,0 @@
-import Blog, {
-  getStaticProps as paginatedGetStaticProps,
-} from 'pages/blog/p/[page]';
-
-export const getStaticProps = async ({ params, ...other }) =>
-  paginatedGetStaticProps({ ...other, params: { page: '0' } });
-
-export default Blog;
